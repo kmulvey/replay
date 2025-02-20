b6d5834b1b4b7bfca3ceab4ae14db180bb15595b
Kevin Mulvey <kmulvey@linux.com>
2025-02-19 19:58:48 -0700

    nine routes

--- server/main.go
+++ server/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"time"
 
@@ -11,22 +10,49 @@ import (
 func main() {
 	app := fiber.New()
 
-	app.Get("/slow", func(c fiber.Ctx) error {
-		time.Sleep(time.Millisecond * 10)
-		fmt.Println("slow")
-		return c.SendString("slow")
-	})
-
-	app.Get("/medium", func(c fiber.Ctx) error {
-		time.Sleep(time.Millisecond * 5)
-		fmt.Println("medium")
-		return c.SendString("medium")
-	})
-
-	app.Get("/fast", func(c fiber.Ctx) error {
+	app.Get("/one", func(c fiber.Ctx) error {
 		time.Sleep(time.Millisecond)
-		fmt.Println("fast")
-		return c.SendString("fast")
+		return c.SendString("one")
+	})
+
+	app.Get("/two", func(c fiber.Ctx) error {
+		time.Sleep(time.Millisecond * 2)
+		return c.SendString("two")
+	})
+
+	app.Get("/three", func(c fiber.Ctx) error {
+		time.Sleep(time.Millisecond * 3)
+		return c.SendString("three")
+	})
+
+	app.Get("/four", func(c fiber.Ctx) error {
+		time.Sleep(time.Millisecond * 4)
+		return c.SendString("four")
+	})
+
+	app.Get("/five", func(c fiber.Ctx) error {
+		time.Sleep(time.Millisecond * 5)
+		return c.SendString("five")
+	})
+
+	app.Get("/six", func(c fiber.Ctx) error {
+		time.Sleep(time.Millisecond * 6)
+		return c.SendString("six")
+	})
+
+	app.Get("/seven", func(c fiber.Ctx) error {
+		time.Sleep(time.Millisecond * 7)
+		return c.SendString("seven")
+	})
+
+	app.Get("/eight", func(c fiber.Ctx) error {
+		time.Sleep(time.Millisecond * 8)
+		return c.SendString("eight")
+	})
+
+	app.Get("/nine", func(c fiber.Ctx) error {
+		time.Sleep(time.Millisecond * 9)
+		return c.SendString("nine")
 	})
 
 	log.Fatal(app.Listen(":7000", fiber.ListenConfig{
